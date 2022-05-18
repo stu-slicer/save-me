a2c3f3ec7cd50b7c6e2604c50b26092e29a3df86
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-05-18 17:46:27 +0100

    tidy up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css';
-import Button from './components/Button/Button';
-import GameController from './containers/GameController/GameController';
 import LyricList from './containers/LyricList/LyricList';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Save me!</h1>
-      <GameController gameId="Swing" gameLevel="42"  />
       <LyricList />
     </div>
   );
